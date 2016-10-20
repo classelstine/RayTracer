9688491f0b9f7d6c4a1bcc4c4cb88d222c57dce3
Claire Asselstine <claire.asselstine@gmail.com>
2016-10-19 22:37:58 -0700

    should compile

--- src/raytrace.h
+++ src/raytrace.h
@@ -2,29 +2,47 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <valarray>
 
-//include header file for glfw library so that we can use OpenGL
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+//#include <opencv2/opencv.hpp>
 #define PI 3.1415926535
+using namespace std;
 
+class Color {
+    public:
+        float r, g, b;
+        Color(float, float, float);
+        Color();
+        void add_color(Color);
+};
+
+void Color::add_color(Color c) {
+    r = r + c.r;
+    g = g + c.g;
+    b = b + c.b;
+}
+
+// Scene will hold our Film, Camera
 class Scene {
     public:
-        vector eye_position;
-        vector UL;
-        vector UR;
-        vector LL;
-        vector LR;
+        vector<float> eye_position;
+        vector<float> UL;
+        vector<float> UR;
+        vector<float> LL;
+        vector<float> LR;
         float resolution_x;
         float resolution_y;
         Scene();
-        Scene(vector, vector, vector, vector, vector, float, float);
+        Scene(vector<float>, vector<float>, vector<float>, vector<float>, vector<float>, float, float);
+        void initialize();
         void render();
-}
+};
 
-Scene::Scene(vector eye, vector ul, vector ur, vector ll, vector lr, float rx, float ry) {
+Scene::Scene(vector<float> eye, vector<float> ul, vector<float> ur, vector<float> ll, vector<float> lr, float rx, float ry) {
     eye_position = eye;
     UL = ul;
     UR = ur;
@@ -34,13 +52,17 @@ Scene::Scene(vector eye, vector ul, vector ur, vector ll, vector lr, float rx, f
     resolution_y = ry;
 }
 
+void Scene::initialize(void) {
+}
+
+// Stores Screen Coordinate
 class Sample {
     public:
         float x;
         float y;
         Sample();
         Sample(float, float);
-}
+};
 
 Sample::Sample(float my_x, float my_y) {
     x = my_x;
@@ -48,46 +70,38 @@ Sample::Sample(float my_x, float my_y) {
 }
 
 class Sampler {
+    // For now, the sampler just generates a single sample in the center of each pixel. 
     // It will generate (x,y) of a screen sample and return true.
     // Next time it gets called, it will generate another sample for the next pixel. It will return false when all the samples from all the pixels
     // are generated. (In our case, we generate 1 sample per pixel, at the pixel sample. Later on, if we want to do multi-sample per pixel, we need to modify this class.
+    int current_px;
+    int current_py;
     public:
-        bool get_sample(Sample* sample);   
-}
+        bool get_sample(Sample* sample);
+        Sampler();
+};
 
-bool get_sample(Sample* sample);
+//bool Sampler::get_sample(Sample* sample);
 
 class Matrix {
     public:
         float mat[4][4];
         Matrix();
-}
+};
 
 class Film {
-    vector<vector<Color>> pixels;
-    public:
+    // The Film aggregates samples, storing them in buckets (one bucket for each pixel). When it's time to write out the file, the Film averages all the samples in each pixel's bucket to produce that pixel's output color. 
+    // Each pixel is designated one bucket of Color vectors, once all samples are created, each bucket is averaged to create the color for each pixel.
+    vector<vector<vector<Color>>> pixel_buckets;
+       public:
         void commit(Sample& sample, Color& color);
         void write_image();
-        Film();
-}
-
-class Color {
-    public:
-        float r, g, b;
-        Color(float, float, float);
-        Color();
-        void add_color(Color);
-}
-
-void Color::add_color(Color c) {
-    r = r + c.r;
-    g = g + c.g;
-    b = b + c.b;
-}
+        Film(int, int, int);
+};
 
 class Sphere {
     public:
         float radius;
-        vector center;
-}
+        vector<float> center;
+};
 
