8ca7b35efe5147f12381a454583ddfb849303a1b
Claire Asselstine <claire.asselstine@gmail.com>
2016-10-20 12:55:57 -0700

    the house, does not compile

--- src/raytrace.cpp
+++ src/raytrace.cpp
@@ -16,21 +16,73 @@ void Film::write_image(void) {
     cout << "ATTEMPTING TO WRITE IMAGE" << endl;
 }
 
+// Add color c to the bucket of colors for sample s
+void Film::commit(Sample s, Color c) {
+    cout << "Committing color to film" << endl;
+}
+
 void Camera::generate_ray(Sample s, Ray* r) {
     cout << "CAMERA GENERATING RAY" << endl;
 }
 
+// Currently is a dummy function which sets the color to 0.5
 void Raytracer::trace(Ray r, Color *c) {
     cout << "RAY TRACER TRACING" << endl;
+    c->r = 0.5;
+    c->g = 0.5;
+    c->b = 0.5;
+}
+
+Sampler::Sampler(void) {
+    current_px = 0;
+    current_py = 0;
+    max_x = 100;
+    max_y = 100;
+}
+
+Sampler::Sampler(int x_res, int y_res) {
+    current_px = 0;
+    current_py = 0;
+    max_x = x_res-1;
+    max_y = y_res-1;
+}
+
+
+bool Sampler::get_sample(Sample *sample){
+    // set this sample to the current sample (in screen coords)
+    sample->x = current_px;
+
+    sample->y = current_py;
+    // update next sample
+    if (current_px < max_x) {
+        current_px += 1;
+    } else if (current_py < max_y) {
+        current_px = 0;
+        current_py += 1;
+    } else {
+        return false;
+    }
+    return true
+        
+
+}
+
+// Clear all pixel buckets
+void Scene::initialize(void) {
+    cout << "Initializing Scene" << endl;
 }
 
 void Scene::render(void) {
     // This is the main rendering loop
-    while(!sampler.get_sample(&sample) {
-          camera.generate_ray(sample, &ray);
-          raytracer.trace(ray, &color);
-          film.commit(sample, color);
-        }
+    Sample sample = Sample();
+    while(!sampler.get_sample(&sample)) {
+        Ray ray = Ray();
+        camera.generate_ray(sample, &ray);
+        Color color = Color();
+        raytracer.trace(ray, &color);
+        film.commit(sample, color);
+        Sample sample = Sample();
+    }
     film.write_image();
 }
 
--- src/raytrace.h
+++ src/raytrace.h
@@ -12,6 +12,14 @@
 #define PI 3.1415926535
 using namespace std;
 
+// Ray class represented as a point and a direction
+class Ray {
+    public:
+        float px, py, pz, dx, dy, dz;
+        Ray();
+        Ray(float, float, float, float, float, float);
+};
+
 class Color {
     public:
         float r, g, b;
@@ -25,7 +33,72 @@ void Color::add_color(Color c) {
     g = g + c.g;
     b = b + c.b;
 }
+// Stores Screen Coordinate
+class Sample {
+    public:
+        float x;
+        float y;
+        Sample();
+        Sample(float, float);
+};
 
+Sample::Sample(float my_x, float my_y) {
+    x = my_x;
+    y = my_y;
+}
+
+class Sampler {
+    // For now, the sampler just generates a single sample in the center of each pixel. 
+    // It will generate (x,y) of a screen sample and return true.
+    // Next time it gets called, it will generate another sample for the next pixel. It will return false when all the samples from all the pixels
+    // are generated. (In our case, we generate 1 sample per pixel, at the pixel sample. Later on, if we want to do multi-sample per pixel, we need to modify this class.
+    int current_px;
+    int current_py;
+    int max_x;
+    int max_y;
+    public:
+        bool get_sample(Sample* sample);
+        Sampler(int x_res, int y_res);
+        Sampler();
+};
+
+//bool Sampler::get_sample(Sample* sample);
+
+class Matrix {
+    public:
+        float mat[4][4];
+        Matrix();
+};
+
+class Film {
+    // The Film aggregates samples, storing them in buckets (one bucket for each pixel). When it's time to write out the file, the Film averages all the samples in each pixel's bucket to produce that pixel's output color. 
+    // Each pixel is designated one bucket of Color vectors, once all samples are created, each bucket is averaged to create the color for each pixel.
+    vector<vector<vector<Color>>> pixel_buckets;
+       public:
+        void commit(Sample& sample, Color& color);
+        void write_image();
+        Film(int, int, int);
+};
+
+class Sphere {
+    public:
+        float radius;
+        vector<float> center;
+};
+
+class Raytracer {
+    public:
+        void trace(Ray, Color&);
+        Raytracer();
+};
+
+// Camera class, which can take a sample's coordinates and create a ray from the eye location through this point
+// in the image. 
+class Camera {
+    public:
+        Camera();
+        void generate_array(Sample cur_sample, Ray& cur_ray);
+};
 // Scene will hold our Film, Camera
 class Scene {
     Sampler sampler;
@@ -59,74 +132,4 @@ Scene::Scene(vector<float> eye, vector<float> ul, vector<float> ur, vector<float
 void Scene::initialize(void) {
 }
 
-// Stores Screen Coordinate
-class Sample {
-    public:
-        float x;
-        float y;
-        Sample();
-        Sample(float, float);
-};
-
-Sample::Sample(float my_x, float my_y) {
-    x = my_x;
-    y = my_y;
-}
-
-class Sampler {
-    // For now, the sampler just generates a single sample in the center of each pixel. 
-    // It will generate (x,y) of a screen sample and return true.
-    // Next time it gets called, it will generate another sample for the next pixel. It will return false when all the samples from all the pixels
-    // are generated. (In our case, we generate 1 sample per pixel, at the pixel sample. Later on, if we want to do multi-sample per pixel, we need to modify this class.
-    int current_px;
-    int current_py;
-    public:
-        bool get_sample(Sample* sample);
-        Sampler();
-};
-
-//bool Sampler::get_sample(Sample* sample);
-
-class Matrix {
-    public:
-        float mat[4][4];
-        Matrix();
-};
-
-class Film {
-    // The Film aggregates samples, storing them in buckets (one bucket for each pixel). When it's time to write out the file, the Film averages all the samples in each pixel's bucket to produce that pixel's output color. 
-    // Each pixel is designated one bucket of Color vectors, once all samples are created, each bucket is averaged to create the color for each pixel.
-    vector<vector<vector<Color>>> pixel_buckets;
-       public:
-        void commit(Sample& sample, Color& color);
-        void write_image();
-        Film(int, int, int);
-};
-
-class Sphere {
-    public:
-        float radius;
-        vector<float> center;
-};
-
-class Raytracer {
-    public:
-        void trace(Ray, *Color);
-        Raytracer();
-}
-
-// Camera class, which can take a sample's coordinates and create a ray from the eye location through this point
-// in the image. 
-class Camera {
-    public:
-        Camera();
-        void generate_array(Sample cur_sample, Ray& cur_ray);
-}
-
-// Ray class represented as a point and a direction
-class Ray {
-    public:
-        float px, py, pz, dx, dy, dz;
-        Ray(float, float, float, float, float, float);
-}
 
